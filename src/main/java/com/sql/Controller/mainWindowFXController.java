271429f17d089b2928a7516d8a4d70170c980fd9
absaboss <absalon.deel@laposte.net>
2015-11-30 11:56:20 +0100

    Nouveau projet.

---
+++ src/main/java/com/sql/Controller/mainWindowFXController.java
@@ -0,0 +1,11 @@
+package com.sql.Controller;
+
+import javafx.event.ActionEvent;
+
+/**
+ * Created by Absalon DEEL on 30/11/2015.
+ */
+public class mainWindowFXController {
+    public void handleSubmitButtonAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/sql/View/mainWindowFX.java
@@ -0,0 +1,31 @@
+package com.sql.View;/**
+ * Created by Absalon DEEL on 30/11/2015.
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class mainWindowFX extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private GridPane mainPane;
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        mainPane = FXMLLoader.load(getClass().getResource("mainWindow.fxml"));
+
+        Scene scene = new Scene(mainPane, 400, 350);
+
+        primaryStage.setTitle("Restaurants Manager");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
