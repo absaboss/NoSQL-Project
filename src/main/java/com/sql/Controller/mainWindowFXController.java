5beca0b0e28ddf29c5206c8ef8d547a8bbbd98f2
absaboss <absalon.deel@laposte.net>
2015-12-01 15:50:51 +0100

    Petit changement, la connexion à mongo se fait une seule fois mainenant donc les requêtes sont un peu plus rapide

--- src/main/java/com/sql/Controller/mainWindowFXController.java
+++ src/main/java/com/sql/Controller/mainWindowFXController.java
@@ -3,18 +3,37 @@ package com.sql.Controller;
 import com.sql.Model.MongoDBClient;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
-public class mainWindowFXController {
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class mainWindowFXController implements Initializable {
 
     @FXML private TextField inputVal;
 
+    MongoDBClient mongoClient;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        mongoClient = new MongoDBClient();
+    }
+
     public void handleSubmitButtonAction(ActionEvent actionEvent) {
-        MongoDBClient mongoClient = new MongoDBClient();
+
 
         String restaurantName = inputVal.getText().toString();
 
-        mongoClient.affichage(mongoClient.findByName(restaurantName));
+//        mongoClient.affichage(mongoClient.findByName(restaurantName));
+
+        List<String> stringList = mongoClient.findByBorough(restaurantName);
+
+        //On aussi faire un tirage aléatoire pour savoir où on commence
+        for(int i = 0; i <5;i++){//On affiche seulement 5
+            mongoClient.affichage(stringList.get(i));
+        }
     }
 }
