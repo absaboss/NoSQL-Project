56428d8ee64bdb62c2b29583837602bdedebcddd
Adrien <biaku@hotmail.fr>
2015-12-04 09:48:34 +0100

    Merge branch 'master' of D:\ESILV\Cours\S07\Software development process\NoSQL with conflicts.

--- src/main/java/com/sql/Controller/searchCuisineFXController.java
+++ src/main/java/com/sql/Controller/searchCuisineFXController.java
@@ -1,18 +1,31 @@
 package com.sql.Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.GridPane;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 /**
  * Created by Absalon DEEL on 02/12/2015.
  */
-public class searchCuisineFXController extends mainWindowFXController{
+public class countRestaurantFXController implements Initializable {
 
+    @FXML private GridPane grid;
+    private ComboBox<String> comboCuisine;
+    private ComboBox<String> comboBorough;
 
-    public void handleSearchCButtonAction(ActionEvent actionEvent) {
-        mongoClient.allBorough();
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        //List<String> list = // requette
+
+        for
+
     }
 }
